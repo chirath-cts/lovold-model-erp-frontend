e118c9181c8a5bdeeb101f106903097f20b4ac74
Chirath Perera <chirath.p@creativesoftware.com>
2026-03-13 17:41:10 +0530

    Toastify component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { ToastContainer } from "react-toastify";
 import EnvironmentLabel from "./components/EnvironmentLabel/EnvironmentLabel";
 
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <ToastContainer position="top-right" autoClose={3000}/>
       <EnvironmentLabel />
     </>
   );
